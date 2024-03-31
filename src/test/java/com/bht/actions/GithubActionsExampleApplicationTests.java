ec89c74ebdb6f277f692ed6cd5d8d94f7e7089df
hariteja <bandlahariteja0696@gmail.com>
2024-03-31 10:24:59 +0530

    first commit

---
+++ src/test/java/com/bht/actions/GithubActionsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bht.actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
